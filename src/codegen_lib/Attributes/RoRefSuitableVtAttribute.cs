53f0800b8f5243b1fb03be708d06fd460b13d8ee
Christopher P. Susie <cpsusie@hotmail.com>
2021-09-13 14:49:08 -0400

    Add attributes.

---
+++ src/codegen_lib/Attributes/RoRefSuitableVtAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cjm.CodeGen.Attributes
+{
+    [AttributeUsage(AttributeTargets.GenericParameter)]
+    public sealed class RoRefSuitableVtAttribute : Attribute
+    {
+        public const string ShortName = "RoRefSuitableVt";
+    }
+
+    [AttributeUsage(AttributeTargets.GenericParameter)]
+    public sealed class ClassNotInterfaceAttribute : Attribute
+    {
+        public const string ShortName = "NotInterface";
+    }
+
+    [AttributeUsage(AttributeTargets.GenericParameter)]
+    public sealed class RefTypeOrSmallValueTypeAttribute : Attribute
+    {
+        public const string ShortName = "ClassOrSmallStruct";
+    }
+
+}
